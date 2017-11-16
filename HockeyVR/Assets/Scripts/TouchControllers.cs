85487491973dca036d4d57cda888a83cea372f42
notthewarriorprincess <notthewarriorprincess@gmail.com>
2017-11-16 14:06:47 -0700

    logs buttons?

---
+++ HockeyVR/Assets/Scripts/TouchControllers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControllers : MonoBehaviour {
+
+    public OVRInput.Controller controller;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (OVRInput.Get(OVRInput.Button.One, controller))
+        {
+            Debug.Log("Pressed A!");
+        }
+	}
+}
