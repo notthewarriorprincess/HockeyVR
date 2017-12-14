b4457b70c244fd2b83126ba53accd40bf4e79f30
notthewarriorprincess <notthewarriorprincess@gmail.com>
2017-12-14 15:44:50 -0700

    SCOREBOARD!!!

---
+++ HockeyVR/Assets/Scripts/Net.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Net : MonoBehaviour {
+
+    public Scoreboard scoreboard;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnCollisionEnter(Collision collider)
+    {
+        if (collider.gameObject.name == "Puck2")
+        {
+            scoreboard.homeScore.text += 1;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ HockeyVR/Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoreboard : MonoBehaviour {
+
+    public Text homeScore;
+    [SerializeField]
+    private Text timeText;
+
+    private bool gameOn = true;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("updateTime");
+    }
+
+    IEnumerator updateTime()
+    {
+        while (gameOn)
+        {
+            var today = System.DateTime.Now;
+            timeText.text = today.ToString("HH:mm");
+            yield return new WaitForSeconds(5);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
